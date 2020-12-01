ee9a155fa1aec20ea25616907a2fe6d39968793c
Thomas Hart <thh4002@med.cornell.edu>
2020-12-01 00:19:23 +0000

    added formatting function

---
+++ format-df.sh
@@ -0,0 +1,18 @@
+# inputs
+dockerfile_in=$1
+dockerfile_out=$1
+
+if [[ $dockerfile_out = '' ]];
+then
+    dockerfile_out=biohpc-$dockerfile_in
+fi
+
+# docstring
+echo -e \
+    "converting $dockerfile_in to biohpc valid dockerfile\n \
+     and writing converted file out to $dockerfile_out"
+
+# body
+cat $dockerfile_in \
+    | sed -r "s/FROM ([[:graph:]]*)/FROM biohpc_$USER\/\1/" \
+    > $dockerfile_out
